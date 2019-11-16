4332e2938c2e55a328e88559d710c883edeb9532
dhillon-Prabh <psingh143105@gmail.com>
2019-11-16 12:36:12 -0800

    entity beans ready

--- JavaSource/com/corejsf/model/Timesheet.java
+++ JavaSource/com/corejsf/model/Timesheet.java
@@ -7,6 +7,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
@@ -20,17 +21,57 @@ public class Timesheet {
     @Column(name="TimesheetID")
     private int id;
     
-    @OneToMany
+    @ManyToOne
     @JoinColumn(name="EmployeeID")
     private String empID;
 
     @Column(name="EndWeek")
     private Date endWeek;
     
-    @Column(name="")
-    private Date endWeek;
+    @Column(name="Overtime")
+    private float overtime;
     
-    @Column(name="EndWeek")
-    private Date endWeek;
+    @Column(name="Flextime")
+    private float flextime;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmpID() {
+        return empID;
+    }
+
+    public void setEmpID(String empID) {
+        this.empID = empID;
+    }
+
+    public Date getEndWeek() {
+        return endWeek;
+    }
+
+    public void setEndWeek(Date endWeek) {
+        this.endWeek = endWeek;
+    }
+
+    public float getOvertime() {
+        return overtime;
+    }
+
+    public void setOvertime(float overtime) {
+        this.overtime = overtime;
+    }
+
+    public float getFlextime() {
+        return flextime;
+    }
+
+    public void setFlextime(float flextime) {
+        this.flextime = flextime;
+    }
     
 }
--- JavaSource/com/corejsf/model/TimesheetRow.java
+++ JavaSource/com/corejsf/model/TimesheetRow.java
@@ -1,5 +1,146 @@
 package com.corejsf.model;
 
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
 public class TimesheetRow {
+
+    @Id
+    @Column(name="RowID")
+    private int id;
+    
+    @ManyToOne
+    @JoinColumn(name="TimesheetID")
+    private String timesheetID;
+
+    @Column(name="ProjectID")
+    private String projectID;
+
+    @Column(name="WorkPackage")
+    private String wp;
+
+    @Column(name="HourMon")
+    private String hourMon;
+
+    @Column(name="HourTue")
+    private String hourTue;
+
+    @Column(name="HourWed")
+    private String hourWed;
+
+    @Column(name="HourThur")
+    private String hourThur;
+
+    @Column(name="HourFri")
+    private String hourFri;
+
+    @Column(name="HourSat")
+    private String hourSat;
+
+    @Column(name="HourSun")
+    private String hourSun;
+
+    @Column(name="Note")
+    private String note;
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTimesheetID() {
+        return timesheetID;
+    }
+
+    public void setTimesheetID(String timesheetID) {
+        this.timesheetID = timesheetID;
+    }
+
+    public String getProjectID() {
+        return projectID;
+    }
+
+    public void setProjectID(String projectID) {
+        this.projectID = projectID;
+    }
+
+    public String getWp() {
+        return wp;
+    }
+
+    public void setWp(String wp) {
+        this.wp = wp;
+    }
+
+    public String getHourMon() {
+        return hourMon;
+    }
+
+    public void setHourMon(String hourMon) {
+        this.hourMon = hourMon;
+    }
+
+    public String getHourTue() {
+        return hourTue;
+    }
+
+    public void setHourTue(String hourTue) {
+        this.hourTue = hourTue;
+    }
+
+    public String getHourWed() {
+        return hourWed;
+    }
+
+    public void setHourWed(String hourWed) {
+        this.hourWed = hourWed;
+    }
+
+    public String getHourThur() {
+        return hourThur;
+    }
+
+    public void setHourThur(String hourThur) {
+        this.hourThur = hourThur;
+    }
+
+    public String getHourFri() {
+        return hourFri;
+    }
+
+    public void setHourFri(String hourFri) {
+        this.hourFri = hourFri;
+    }
+
+    public String getHourSat() {
+        return hourSat;
+    }
+
+    public void setHourSat(String hourSat) {
+        this.hourSat = hourSat;
+    }
+
+    public String getHourSun() {
+        return hourSun;
+    }
+
+    public void setHourSun(String hourSun) {
+        this.hourSun = hourSun;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
 
 }
